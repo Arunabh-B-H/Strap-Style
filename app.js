ca8f21987f73099af3b6853fd057c2f12ce8d752
Arunabh BHR <arunabh2023@gmail.com>
2025-08-31 01:44:49 +0530

    completed user and product models

--- app.js
+++ app.js
@@ -11,5 +11,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("hey");
+});
 app.listen(3000);
