678c9c1dbd48aaf0ca62b9d5899624fe1b7e43c7
Arunabh BHR <arunabh2023@gmail.com>
2025-08-31 04:35:09 +0530

    handled error

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,2 +1,9 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/styleandstrap");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/styleandstrap")
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
